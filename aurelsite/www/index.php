fd99948dcdb57323e34f2c753563d85741032236
Sébastien Ferreira <ferreirasebastien78580@gmail.com>
2019-02-13 17:42:18 +0100

    MAJ enquete

--- aurelsite/www/index.php
+++ aurelsite/www/index.php
@@ -1,70 +1,68 @@
 <html>
  <head> 
-<meta http-equiv="content-Type" content="text/html; charset=iso-8859-1"> 
-<link rel="stylesheet" type="text/css" href="index.css" />
+     <meta http-equiv="content-Type" content="text/html; charset=iso-8859-1"> 
+     <link rel="stylesheet" type="text/css" href="index.css" />
 </head>
 
 <body>
 <h4>EN-TETE</h4>
 
 <div class="gauche">
+    <center>
+        <br>
+        <br>
+        <br>
     <div>
         <a target="_blank"><img border="0" src="http://www.calendian.com/gen/fr.png" alt="Calendrier - Français, Calendian"></a>
     </div>
-    
+    <br>
+        <br>
+        <br>
     <div>
     <!-- weather widget start --><a target="_blank" href="https://hotelmix.fr/weather/paris-18145">
         <img src="https://w.bookcdn.com/weather/picture/31_18145_1_3_c55a99_250_bf428c_ffffff_ffffff_1_2071c9_ffffff_0_6.png?scode=124&domid=581&anc_id=54502" alt="booked.net"/></a><!-- weather widget end -->
     </div>
+        </center>
         
 </div>
 
 <div class="droite"> <!-- Dans certains cas, mettre la colonne droite avant la centre passe mieux -->
-    <a href="https://www.norauto.fr/"><img src="norauto.gif" alt= "norauto"></a>
+    <center>
+        <br>
+        <br>
+        <br>
+        <a href="https://www.norauto.fr/"><img src="norauto.gif" alt= "norauto"></a>
+    </center>
 </div>
     
 <div class="centre"> 
-        <script>
-
-        function verif(){
-
-            var email=document.getElementById("email").value;
-            var cptad=0;
-            var cptpoint=0;
-
-            for (var i=0; i<email.length; i++)
-            {
-                if (email.charAt(i)=='@')
-                {
-                    cptad++;
-
-                }
-                else if (email.charAt(i)=='.')
-                {
-                    cptpoint++;
-                }
-            }
-            if(cptad==1 && cptpoint>=1){}
-            else
-            {
-                alert("email saisie invalide ");
-                return false;
-            }
-        }
-
-    </script>
-
-    <center>
-        <h2>Enquête de satisfaction</h2> <br>
-        <form method="POST" onSubmit="return verif()" action="page3.html">
-            nom: <input id="nom" required/> <br><br>
-            prenom: <input id="prenom" required/> <br><br>
-            email: <input id="email"required/> <br><br>
-            Date de naissance: <input type="date" id="date" name = "date" required>
-            <br><br>
-            <input type="submit" value = "Suivant" >
-        </form>
-    </center>
+        
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://www.google.com/search?q=photo+mercedes&client=firefox-b-d&tbm=isch&source=iu&ictx=1&fir=SZCGjl2-cHHj0M%253A%252Cdqgjz_dlj5IskM%252C_&usg=AI4_-kT2m1u9BLqBdO653-MwOvfnnMLcAA&sa=X&ved=2ahUKEwilmfHWkrngAhXT5-AKHRooDYYQ9QEwAHoECAQQBA&biw=1920&bih=1005#imgrc=SZCGjl2-cHHj0M:" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
     </div>
 <h4>PIED-DE-PAGE</h4>
 
